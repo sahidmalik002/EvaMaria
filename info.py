9f2850e136a73b0be48d7a3089500d0a1bfdfd6a
sahidmalik002 <sn.malik7283@gmail.com>
2024-11-12 20:17:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mynewdbindex')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
